ffda05c3c6beafe6d304c80f7d9a0380232a8a32
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2023-11-05 15:27:41 +0530

    Day 1 tasks complete

---
+++ Challenges/Day_1/day_1_sollution.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Task 1
+# This is solution for Day 1 Tasks
+
+#Task 2"
+echo "Hello Everyone"
+
+#Task 3
+result=0
+
+#Task 4
+echo "Enter the first Number"
+read a
+recho "Enter the second number"
+read b
+
+result=$a+$b
+
+echo "Sum of the two numbers is $result"
+
+#Task 5
+echo "My home directory - $HOME"
+echo "Where am I currently? - $PWD"
+echo "My hostname - $HOSTNAME"
+
+#Task 6
+echo "List of all text files in this directory"
+
+ls -l *.txt
