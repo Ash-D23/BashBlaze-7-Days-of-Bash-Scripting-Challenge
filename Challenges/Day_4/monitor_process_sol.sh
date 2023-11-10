5bc2c4d38ae418c3ed9bc6cb5a8e8cd97c7730b8
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2023-11-10 21:24:26 +0530

    added day 4 part 1

---
+++ Challenges/Day_4/monitor_process_sol.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+restart_process(){
+    process_name=$1
+
+    attempts=0
+    status=0
+
+    while [ $attempts -le 3 ] && [ $status -eq 0 ]
+    do
+        echo "Starting the service"
+        $(sudo systemctl start nginx )
+        sleep 2
+        status=$(pgrep $1 | wc -l)
+        attempts=`expr $attempts + 1`
+    done
+
+    status=$(pgrep $1 | wc -l)
+    if [ $status -gt 0 ]
+    then
+            echo "Process restarted succesfully"
+    else
+            echo "Multiple Attempts unsuccesfull"
+    fi
+}
+
+#check if the arg is present
+if [ -z $1 ]
+then
+        echo "Pass the process name as argument"
+else
+        #check if the process is running
+        status=$(pgrep $1 | wc -l)
+        if [ $status -gt 0 ]
+        then
+                echo "Process is running"
+        else
+                restart_process $1
+        fi
+fi
